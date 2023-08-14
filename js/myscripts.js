ab5d18c53018476579ac479bb7fb247b5dd39879
yassin.akhayad <yassin.akhayad@cegeka.com>
2023-08-14 13:18:10 +0200

    add handling message from iframe

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,1 +1,13 @@
 
+window.addEventListener("message", receiveMessage, false);
+
+function receiveMessage(event) {
+    // Check the origin to ensure it's from your subdomain
+    if (event.origin !== "https://yellow-moss-00f964103.3.azurestaticapps.net") {
+        return;
+    }
+
+    // Handle the message content
+    var data = event.data;
+    console.log(data);
+}

7119eaf6b01724b53bb2acb0860f2542769cacbb
yassin.akhayad <yassin.akhayad@cegeka.com>
2023-08-14 13:44:13 +0200

    more logging

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,8 +1,15 @@
 function handleAuthentication(){
+    alert("handle auth");
+    console.log({urlFragment});
     const urlFragment = window.location.hash;
     if (urlFragment.includes("id_token")) {
         // Authentication was successful. Extract and handle the ID token.
         console.log("token found");
+
+        let iframes = document.querySelectorAll('iframe');
+        for (let i = 0; i < iframes.length; i++) {
+            iframes[i].parentNode.removeChild(iframes[i]);
+        }
     }
 }
 

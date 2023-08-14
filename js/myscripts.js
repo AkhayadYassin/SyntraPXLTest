5d6c7cd27cea5f51196a34be63353cf2739ccc7e
yassin.akhayad <yassin.akhayad@cegeka.com>
2023-08-14 14:05:55 +0200

    fixed handling

--- js/myscripts.js
+++ js/myscripts.js
@@ -4,14 +4,26 @@ function handleAuthentication(){
     if (urlFragment.includes("id_token")) {
         // Authentication was successful. Extract and handle the ID token.
         console.log("token found");
-
-        let iframes = document.querySelectorAll('iframe');
-        for (let i = 0; i < iframes.length; i++) {
-            iframes[i].parentNode.removeChild(iframes[i]);
-        }
+        window.postMessage(urlFragment, "*")
     }
 }
+function receiveMessage(event) {
+    // Check the origin to ensure it's from your subdomain
+    // if (event.origin !== "https://sub.example.com") {
+    //     return;
+    // }
+    console.log(event.origin);
 
+    // Handle the message content
+    var data = event.data;
+    console.log(data);
+
+    let iframes = document.querySelectorAll('iframe');
+    for (let i = 0; i < iframes.length; i++) {
+        iframes[i].parentNode.removeChild(iframes[i]);
+    }
+}
+window.addEventListener("message", receiveMessage, false);
 window.addEventListener("load", (event) => {
     handleAuthentication();
 });

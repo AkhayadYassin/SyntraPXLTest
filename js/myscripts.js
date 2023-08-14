bcfaa8b63c5322a57e384d5a9d041b12e97a5494
yassin.akhayad <yassin.akhayad@cegeka.com>
2023-08-14 13:42:43 +0200

    fixed handling of events

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,19 +1,9 @@
-
-window.addEventListener("message", receiveMessage, false);
-
-function receiveMessage(event) {
-    // // Check the origin to ensure it's from your subdomain
-    // if (event.origin !== "https://yellow-moss-00f964103.3.azurestaticapps.net") {
-    //     return;
-    // }
-    if(event){
-        let iframes = document.querySelectorAll('iframe');
-        for (let i = 0; i < iframes.length; i++) {
-            iframes[i].parentNode.removeChild(iframes[i]);
-        }
+function handleAuthentication(){
+    const urlFragment = window.location.hash;
+    if (urlFragment.includes("id_token")) {
+        // Authentication was successful. Extract and handle the ID token.
+        console.log("token found");
     }
-    // Handle the message content
-    let data = event.data;
-    console.log({event});
-    console.log({data});
 }
+
+handleAuthentication();

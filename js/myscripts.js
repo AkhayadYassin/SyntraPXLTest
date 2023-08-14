172fb7cc0581594e6e07187fe2053ecb3a3d6d8d
yassin.akhayad <yassin.akhayad@cegeka.com>
2023-08-14 13:35:16 +0200

    fixed handling of events

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,12 +2,18 @@
 window.addEventListener("message", receiveMessage, false);
 
 function receiveMessage(event) {
-    // Check the origin to ensure it's from your subdomain
-    if (event.origin !== "https://yellow-moss-00f964103.3.azurestaticapps.net") {
-        return;
+    // // Check the origin to ensure it's from your subdomain
+    // if (event.origin !== "https://yellow-moss-00f964103.3.azurestaticapps.net") {
+    //     return;
+    // }
+    if(event){
+        let iframes = document.querySelectorAll('iframe');
+        for (let i = 0; i < iframes.length; i++) {
+            iframes[i].parentNode.removeChild(iframes[i]);
+        }
     }
-
     // Handle the message content
-    var data = event.data;
-    console.log(data);
+    let data = event.data;
+    console.log({event});
+    console.log({data});
 }
